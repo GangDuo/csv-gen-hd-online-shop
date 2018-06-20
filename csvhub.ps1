dcb81f7592176900db03ce9e43c385c8b7cd2229
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2018-06-20 10:23:12 +0900

    null非許容カラムに0を出力

--- csvhub.ps1
+++ csvhub.ps1
@@ -12,31 +12,31 @@ $script:out = "$($env:USERPROFILE)\Desktop\$($script:key).csv"
 
 Get-Content $Args[0] | `
 ConvertFrom-CSV | `
-Select-Object @{Name='f1';Expression={$script:key}}, `
-@{Name='f2';Expression={(++$script:i)}}, `
-@{Name='f3';Expression={''}}, `
-@{Name='f4';Expression={''}}, `
-@{Name='f5';Expression={''}}, `
-@{Name='出荷元';Expression={'9998'}}, `
-@{Name='店舗';Expression={'401'}}, `
-@{Name='f7';Expression={''}}, `
-@{Name='f8';Expression={''}}, `
-@{Name='f9';Expression={''}}, `
-@{Name='f10';Expression={''}}, `
-@{Name='f11';Expression={''}}, `
-@{Name='f12';Expression={''}}, `
-'JANコード', `
-'発注予定数', `
-@{Name='f13';Expression={''}}, `
-@{Name='f14';Expression={''}}, `
-@{Name='f15';Expression={''}}, `
-@{Name='f16';Expression={''}}, `
-@{Name='f17';Expression={''}}, `
-@{Name='更新フラグ';Expression={'1'}}, `
-@{Name='総数量';Expression={$script:total}}, `
-@{Name='仕入単価';Expression={''}}, `
-@{Name='f18';Expression={'0'}}, `
-@{Name='f19';Expression={'0'}} | `
+Select-Object @{Name='伝票番号';Expression={$script:key}}, `
+			  @{Name='伝票行番号';Expression={(++$script:i)}}, `
+			  @{Name='f3';Expression={''}}, `
+			  @{Name='f4';Expression={''}}, `
+			  @{Name='f5';Expression={''}}, `
+			  @{Name='出荷元';Expression={'9998'}}, `
+			  @{Name='店舗';Expression={'401'}}, `
+			  '仕入先コード', `
+			  @{Name='アイテム';Expression={'0'}}, `
+			  @{Name='品番';Expression={'0'}}, `
+			  @{Name='品名';Expression={'0'}}, `
+			  @{Name='色コード';Expression={'0'}}, `
+			  @{Name='サイズコード';Expression={'0'}}, `
+			  'JANコード', `
+			  '発注予定数', `
+			  @{Name='上代単価';Expression={'0'}}, `
+			  @{Name='下代単価';Expression={'0'}}, `
+			  @{Name='作成日付';Expression={'0'}}, `
+			  @{Name='f16';Expression={''}}, `
+			  @{Name='f17';Expression={''}}, `
+			  @{Name='更新フラグ';Expression={'1'}}, `
+			  @{Name='総数量';Expression={$script:total}}, `
+			  @{Name='仕入単価';Expression={'0'}}, `
+			  @{Name='商品区分';Expression={'0'}}, `
+			  @{Name='客注区分';Expression={'0'}} | `
 ConvertTo-Csv -NoTypeInformation | `
 Select -Skip 1 | `
 % {$_.Replace('"','')} | `
